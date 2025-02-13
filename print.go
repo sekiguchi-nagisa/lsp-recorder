8eb4cbcac88b3bf0e1e14587a3f20ee1e9c62ee2
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2025-02-13 22:34:44 +0900

    increase scan buffer size

--- print.go
+++ print.go
@@ -9,6 +9,8 @@ import (
 
 func Print(reader io.Reader, writer io.Writer) error {
 	scanner := bufio.NewScanner(reader)
+	buf := make([]byte, 1024*16)
+	scanner.Buffer(buf, 1024*1024*64)
 	for scanner.Scan() {
 		logRecord := LogData{}
 		err := json.Unmarshal([]byte(scanner.Text()), &logRecord)
